2e104a2d202d620f67744884e158e8977a3de24a
JMEChristian <jmechristian@gmail.com>
2018-05-09 20:22:05 -0500

    5.9.2018 Set Up Chekout Summary

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+import classes from './CheckoutSummary.css';
+
+const checkoutSummary = (props) => {
+    return (
+        <div className={classes.CheckoutSummary}>
+            <h1>Hope You Enjoyed it Fatty</h1>
+            <div style={{width: '100%', height: '300px !important', margin: 'auto'}}>
+                <Burger ingredients={props.ingredients}/>
+            </div>
+            <Button 
+                btnType="Danger"
+                clicked>CANEL</Button>
+            <Button 
+                btnType="Success"
+                clicked>CONTINUE</Button>
+        </div>
+    )
+}
+
+export default checkoutSummary;
